7dc3420463b40518b13a4ee068de354c39b6dc7a
ajg8669 <ajg8669@uncw.edu>
2020-05-09 12:06:15 -0700

    updated gans

--- Scripts/basic_gan.py
+++ Scripts/basic_gan.py
@@ -9,11 +9,10 @@ import torchvision.datasets as datasets
 from torchvision import transforms
 
 # random noise
-def create_noise():
+def create_noise(batch_size):
     return torch.rand(batch_size,100)
 
-# define generator class
-
+# define generator class=
 class generator(nn.Module):
     
     def __init__(self, inp, out):
@@ -35,7 +34,6 @@ class generator(nn.Module):
         return x
 
 # define discriminator class
-
 class discriminator(nn.Module):
     
     def __init__(self, inp, out):
@@ -57,9 +55,7 @@ class discriminator(nn.Module):
         x = self.net(x)
         return x
 
-
 # plot generated results
-
 def plot_img(array,number=None):
     array = array.detach()
     array = array.reshape(28,28)
@@ -71,78 +67,81 @@ def plot_img(array,number=None):
         plt.xlabel(number,fontsize='x-large')
     plt.show()
 
-# define parameters
+def main():
+    # define parameters
+    gen = generator(100,784)# 1024?
+    dis = discriminator(784,1) #1024?
 
-gen = generator(100,784)# 1024?
-dis = discriminator(784,1) #1024?
+    d_steps = 100
+    g_steps = 100
 
-d_steps = 100
-g_steps = 100
+    criteriond1 = nn.BCELoss()
+    optimizerd1 = optim.SGD(dis.parameters(), lr=0.001, momentum=0.9)
 
-criteriond1 = nn.BCELoss()
-optimizerd1 = optim.SGD(dis.parameters(), lr=0.001, momentum=0.9)
+    criteriond2 = nn.BCELoss()
+    optimizerd2 = optim.SGD(gen.parameters(), lr=0.001, momentum=0.9)
 
-criteriond2 = nn.BCELoss()
-optimizerd2 = optim.SGD(gen.parameters(), lr=0.001, momentum=0.9)
+    printing_steps = 200
 
-printing_steps = 200
+    epochs = 50
 
-epochs = 50
+    # training
 
-# training
-
-for epoch in range(epochs):
-    
-    print epoch
-
-    # training discriminator
-    for d_step in range(d_steps):
-        dis.zero_grad()
+    for epoch in range(epochs):
         
-        # training discriminator on real data
-        for inp_real,_ in dataloader_mnist_train:
-            inp_real_x = inp_real
-            break
+        print(epoch);
 
-        inp_real_x = inp_real_x.reshape(batch_size,784)
-        dis_real_out = dis(inp_real_x)
-        dis_real_loss = criteriond1(dis_real_out, Variable(torch.ones(batch_size,1)))
-        dis_real_loss.backward()
-
-        # training discriminator on data produced by generator
-        inp_fake_x_gen = make_some_noise()        #output from generator is generated        
-        dis_inp_fake_x = gen(inp_fake_x_gen).detach()
-        dis_fake_out = dis(dis_inp_fake_x)
-        dis_fake_loss = criteriond1(dis_fake_out, Variable(torch.zeros(batch_size,1)))
-        dis_fake_loss.backward()
-
-        optimizerd1.step()
-        
-        
+        # training discriminator
+        for d_step in range(d_steps):
+            dis.zero_grad();
             
-    # training generator
-    for g_step in range(g_steps):
-        gen.zero_grad()
-        
-        #generating data for input for generator
-        gen_inp = create_noise()
-        
-        gen_out = gen(gen_inp)
-        dis_out_gen_training = dis(gen_out)
-        gen_loss = criteriond2(dis_out_gen_training, Variable(torch.ones(batch_size,1)))
-        gen_loss.backward()
-        
-        optimizerd2.step()
-        
-    if epoch%printing_steps==0:
-        plot_img(gen_out[0])
-        plot_img(gen_out[1])
-        plot_img(gen_out[2])
-        plot_img(gen_out[3])
-        print("\n\n")
+            #Todo add in data loader
+            # training discriminator on real data
+            for inp_real,_ in dataloader_mnist_train:
+                inp_real_x = inp_real
+                break
+
+            inp_real_x = inp_real_x.reshape(batch_size,784)
+            dis_real_out = dis(inp_real_x)
+            dis_real_loss = criteriond1(dis_real_out, Variable(torch.ones(batch_size,1)))
+            dis_real_loss.backward()
+
+            # training discriminator on data produced by generator
+            inp_fake_x_gen = create_noise()        #output from generator is generated        
+            dis_inp_fake_x = gen(inp_fake_x_gen).detach()
+            dis_fake_out = dis(dis_inp_fake_x)
+            dis_fake_loss = criteriond1(dis_fake_out, Variable(torch.zeros(batch_size,1)))
+            dis_fake_loss.backward()
+
+            optimizerd1.step()
+            
+            
+                
+        # training generator
+        for g_step in range(g_steps):
+            gen.zero_grad()
+            
+            #generating data for input for generator
+            gen_inp = create_noise()
+            
+            gen_out = gen(gen_inp)
+            dis_out_gen_training = dis(gen_out)
+            gen_loss = criteriond2(dis_out_gen_training, Variable(torch.ones(batch_size,1)))
+            gen_loss.backward()
+            
+            optimizerd2.step()
+            
+        if epoch%printing_steps==0:
+            plot_img(gen_out[0])
+            plot_img(gen_out[1])
+            plot_img(gen_out[2])
+            plot_img(gen_out[3])
+            print("\n\n")
 
 
-# produce output
+    # produce output
 
-out = plot_img(gen(create_noise()))
+    out = plot_img(gen(create_noise()))
 
+if __name__ == "__main__":
+    main()
