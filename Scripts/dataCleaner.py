2edad43553c021eccd4c0160b31b6450f28614c1
ajg8669 <ajg8669@uncw.edu>
2020-05-09 14:15:26 -0700

    added in json dump

--- Scripts/dataCleaner.py
+++ Scripts/dataCleaner.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 def unpickle(file):
     import pickle
@@ -6,17 +7,26 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
+def jsonDump(input):
+    import json
+    with open('dataset.json', 'w') as fp:
+        json.dump(input, fp)
+
 def open(filepath):
 
     dataset = {}
     testDataset = {}
     dataLabels = {}
 
+    print(filepath)
+
     for root, dir, files in os.walk(os.path.expanduser(filepath)):
+        print(files)
         for i in files:
+            print(i)
             dataset.update(unpickle(os.path.join(root, i)))
 
     return dataset
 
 if __name__ == "__main__":
-    open()
+    open(sys.argv[1])
